7c94f5874752362e1898aa62b323a2f9b738da32
Fytex <pedropauloqt@gmail.com>
2022-03-17 17:33:03 +0000

    custom csv to json - aggregation functions for lists

--- custom_csv_to_json/src/main.py
+++ custom_csv_to_json/src/main.py
@@ -2,21 +2,35 @@ import re
 
 from sys import version_info
 from itertools import islice
-from typing import Optional, Tuple, Union, List
+from typing import Optional, Tuple, Union, List, Callable
 
 
 if 3 > version_info.major or (3 == version_info.major and 6 >= version_info.minor):
     raise Exception("Python must be >=3.6")
 
 
-file = open("file.csv", encoding='utf8')
+agg_funcs = {
+    'sum': lambda x: str(sum(map(int, x))),
+    'median': lambda x: str(sum(map(int, x))/len(x)),
+    'count': lambda x: str(len(x)), 
+}
+
+
+file = open("file4_sum.csv", encoding='utf8')
 
 header = file.readline()
-item_header_exp = r'([^,{}]+)(?:{(\d+)(?:,(\d+))?})?'
 
-col_exp = fr'(?:{item_header_exp})?(?:,|\n)'
+agg_funcs_exp = '|'.join(agg_funcs.keys())
 
-header_exp_check = fr'^(({item_header_exp})?,)*({item_header_exp})?\n'
+mode_exp = f'(?:::({agg_funcs_exp}))'
+list_exp = fr'(?:{{(\d+)(?:,(\d+))?}}{mode_exp}?)'
+
+item_header_exp = fr'(?:([^,{{}}]+){list_exp}?)'
+
+col_exp = fr'(?:{item_header_exp}?(?:,|\n))'
+
+#header_exp_check = fr'^(({item_header_exp})?,)*({item_header_exp})?\n'
+header_exp_check = fr'{col_exp}+'
 
 if not re.fullmatch(header_exp_check, header):
     raise Exception("Header isn't valid")
@@ -30,7 +44,7 @@ print(col_exp)
 columns = {} # Dict is ordered since Python 3.6
 list_count = 0
 
-for col, min_v, max_v in re.findall(col_exp, header):
+for col, min_v, max_v, mode in re.findall(col_exp, header):
     print(col)
     if not col:
         if list_count == 0:
@@ -45,12 +59,16 @@ for col, min_v, max_v in re.findall(col_exp, header):
             raise Exception(f"Column `{col}` is inside the previous list")
 
         if min_v:
-            counts = (int(min_v), int(max_v or min_v))
-            if counts[1] == 0 or counts[0] > counts[1]:
+            try:
+                opt = (int(min_v), int(max_v or min_v), (mode, agg_funcs[mode]) if mode else None)
+            except KeyError:
+                raise Exception(f"Column `{col}` has invalid mode {mode}")
+            
+            if opt[1] == 0 or opt[0] > opt[1]:
                 raise Exception(f"Column `{col}` must has list with invalid size(s)")
 
-            list_count = counts[1]
-            columns[col] = counts
+            list_count = opt[1]
+            columns[col] = opt
         else:
             columns[col] = None
             
@@ -65,12 +83,16 @@ n_columns = sum(v[1] if v else 1 for v in columns.values())
 
 json_output = []
 
-row_exp_check = fr'^([^,]*,){{{n_columns - 1}}}([^,]*)$'
+item_row_exp = r'([^,]*)(?:,|$)'
+
+row_exp_check = fr'^(?:{item_row_exp}){{{n_columns}}}'
 print(row_exp_check)
+
+item_row_re = re.compile(item_row_exp)
 row_re_check = re.compile(row_exp_check)
 
-item_row_exp = r'([^,]*)(?:,|$)'
-item_row_re = re.compile(item_row_exp)
+
+
 
 for idx, _line in enumerate(file):
     line = _line.rstrip()
@@ -84,19 +106,31 @@ for idx, _line in enumerate(file):
     results = iter(item_row_re.findall(line))
     print(item_row_re.findall(line))
     
-    def get_value(count: Optional[Tuple[int, int]]) -> Union[str, List[str]]:
-        print(count)
-        if count:
-            r = [x for x in islice(results, counts[1]) if x]
+    def get_pair(col: str, opt: Optional[Tuple[int, int, Tuple[str, Callable[[List[str]], str]]]]) -> Union[str, List[str]]:    
+        if opt:
+            
+            min_v, max_v, mode = opt
+            
+            r = [x for x in islice(results, max_v) if x]
 
-            if len(r) < counts[0]:
+            if len(r) < min_v:
                 raise Exception(f"Line {idx + 1} contains a list without enough values")
 
-            return r
+
+            if mode:
+                mode_name, mode_func = mode
+                
+                return (col + '_' + mode_name, mode_func(r))
+            
+            return (col, r)
         else:
-            return next(results)
-        
-    values = {col:get_value(count) for col, count in columns.items()}
+            try:
+                return (col, next(results))
+            except StopIteration:
+                raise Exception(f"Line {idx + 1} doesn't have enough values")
+            
+    
+    values = dict(get_pair(col, opt) for col, opt in columns.items())
     json_output.append(values)
 
 print(json_output)
