3524277ef704aed8d3a27ca7fa885c6083bb8e1d
shiiva03 <unchart6565@gmail.com>
2022-04-16 01:44:37 +0100

    Lexer parsing complete, started yacc

--- ply_simple/interpreter_lex.py
+++ ply_simple/interpreter_lex.py
@@ -1,32 +1,39 @@
 import ply.lex as lex
 import re
 
-tokens = ["BEGINL","STR","LITERAL","IGNORE","TOKENS","PAL","COMMENT","RETURN","REGEXP"]
-literals = ["%","=","[","]",","]
+tokens = ["BEGINL","STR","LITERAL","IGNORE","TOKENS","PAL","COMMENT","RETURN","REGEXP","TVALUE","ERROR","FSTR", "ID","LBUILD","BEGINY"]
+literals = ["%","=","[","]",",","(",")"]
 
 
 def t_BEGINL(t):
     r'%%lexer'
     return t
 
+def t_BEGINY(t):
+    r'%%yacc'
+    return t    
+
 def t_COMMENT(t):
     r'\#\#.*'
     return t
 
 def t_STR(t):
-    r'\"([^"])*\"'
+    r'\"[^\"]+\"'
     return t
 
 def t_LITERAL(t):
     r'%literals'
+    t.value = t.value[1:]
     return t
 
 def t_IGNORE(t):
     r'%ignore'
+    t.value = t.value[1:]
     return t
 
 def t_TOKENS(t):
     r'%tokens'
+    t.value = t.value[1:]
     return t
 
 def t_PAL(t):
@@ -42,7 +49,30 @@ def t_RETURN(t):
     r'return'
     return t
 
+def t_ERROR(t):
+    r'error'
+    return t
+
+
+def t_TVALUE(t):
+    r'(float|int)?\(t.value\)|t.value'
+    return t
+
+def t_FSTR(t):
+    r'f(\".+\"|\'.+\')'
+    return t
+def t_LBUILD(t):
+    r'lex.lex\(\)'
+    return t
+
+def t_ID(t):
+    r'[a-zA-Z]([a-zA-Z_]|\d)*'
+    return t
+
+
 t_ignore = " \t\n"
+
+
 
 
 def t_error(t):
--- ply_simple/interpreter_yacc.py
+++ ply_simple/interpreter_yacc.py
@@ -4,10 +4,14 @@ import sys
 
 
 def p_begin(p):
-    "begin : lexer"
+    "begin : lexer yacc"
 
 def p_lexer(p):
     "lexer : BEGINL declarations"
+    
+def p_yacc(p):
+    "yacc : BEGINY"
+    print("YACC")
 
 def p_declarations(p):
     "declarations : declarations declaration"
@@ -37,16 +41,42 @@ def p_typestokens(p):
     p.parser.string += p[1] + p[2] + p[3] + p[4] + p[5] +"\n"
 
 def p_typesreturn(p):
-    "types : REGEXP RETURN"
-    exp = "r'" + p[1] +"'"+ p[2]+ "\n"
+    "types : REGEXP RETURN '(' PAL ',' TVALUE ')'"
+    exp = construct_function(p)
     p.parser.string += exp
+    
+def construct_function(p):
+    func_name = p[4][1:-1]
+    exp = f"def t_{func_name}(t):\n\tr'{p[1]}'\n\tt.value = {p[6]}\n\treturn t\n"
+    return exp
+
+def p_typeserror(p):
+    "types : REGEXP ERROR '(' elem ',' TVALUE ')' "
+    exp = construct_error_func(p)
+    p.parser.string += exp
+    
+def construct_error_func(p):
+    exp = f"def p_error(t):\n\tprint({p[4]})\n"
+    return exp
+
+def p_typesbuild(p):
+    "types : ID '=' LBUILD "
+    p.parser.string += p[1] + p[2] + p[3] 
+    
+def check_if_present(p,str):
+    tok = str[1:-1]
+    if str[1:-1] in p.parser.tokens:
+        error("Token already in list!", p)
+    p.parser.tokens.append(tok)
     
 def p_contentelem(p):
     "content : elem"
+    check_if_present(p,p[1])
     p[0] = p[1]
 
 def p_contentelems(p):
     "content : content ',' elem"
+    check_if_present(p,p[3])
     p[0] = p[1] + p[2] + p[3]
 
 def p_elem(p):
@@ -56,17 +86,43 @@ def p_elem(p):
 def p_elemSTR(p):
     "elem : STR"
     p[0] = p[1]
+    
+def p_elemFormat(p):
+    "elem : FSTR"
+    p[0] = p[1]
+    
+def error(error,p):
+    print(error)
+    p.parser.success = False
+
+def p_error(p):
+    print("SYNTAX ERROR",p)
+    parser.success = False
 
 
 parser = yacc.yacc()
-parser.string = "import ply.yacc as yacc\nimport ply.lex as lex\n"
-
 
 f = open("test.txt","r")
-fout = open("out.py","w")
+
+parser.string = "import ply.yacc as yacc\nimport ply.lex as lex\n"
+parser.tokens = []
+parser.success = True
 
 content = f.read()
 parser.parse(content)
-print(parser.string,end="")
+print(parser.tokens)
 
-fout.write(parser.string)
+if parser.success:
+    print("Compilation successful!")
+    fout = open("out.py","w")
+    fout.write(parser.string)
+    print(parser.string,end="")
+
+else:
+    print("Compilation failed!")
+
+
+
+
+
+
